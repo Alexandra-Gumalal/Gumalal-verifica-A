db570ceb317179474269e0c31cc8cc18db4140d0
Alexandra Gumalal <gumalal.alexandra@iisgalvanimi.edu.it>
2026-02-06 10:37:10 +0000

    punto 5

--- Gumalal_Alexandra_fila_a/src/app/third-component/third-component.ts
+++ Gumalal_Alexandra_fila_a/src/app/third-component/third-component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FourthComponent } from '../fourth-component/fourth-component';
 @Component({
   selector: 'app-third-component',
-  imports: [],
+  imports: [FourthComponent],
   templateUrl: './third-component.html',
   styleUrl: './third-component.css',
 })
